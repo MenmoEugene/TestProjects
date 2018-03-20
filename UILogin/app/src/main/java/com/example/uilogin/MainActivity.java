9e83229a4753ab439b8eba6b89cc4d588b152553
MenmoEugene <menmo_o@163.com>
2018-03-20 21:21:43 +0800

    updata-UILogin

--- UILogin/app/src/main/java/com/example/uilogin/MainActivity.java
+++ UILogin/app/src/main/java/com/example/uilogin/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText accountEdit;
+    private EditText passwordEdit;
     private Button login;
 
     @Override
@@ -16,11 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         login = (Button) findViewById(R.id.login);
+        accountEdit = (EditText) findViewById(R.id.account);
+        passwordEdit = (EditText) findViewById(R.id.password);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                if (account.equals("menmoxml") && password.equals("123456")) {
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            "account or password is invalid", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
